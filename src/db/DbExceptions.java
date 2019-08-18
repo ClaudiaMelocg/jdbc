49a57becc1ee125272599465886423c813bb479f
Claudia Melo <claudiamelocg@gmail.com>
2019-08-18 11:39:50 -0300

    Projeto Criado

---
+++ src/db/DbExceptions.java
@@ -0,0 +1,16 @@
+package db;
+
+public class DbExceptions extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DbExceptions(String msg) {
+		super(msg);
+		
+	}
+	
+
+}
